1429d123ef9cdf11889c0a0accdda7bed5e6312d
Owen Y. C. <171704567+Owen-y-c@users.noreply.github.com>
2025-06-20 17:43:57 +0800

    feat: Add repetition to game
    Finished the repetition feature that was left to do. Project is
    technically finished according to specifications.

--- script.js
+++ script.js
@@ -13,30 +13,42 @@ function getHumanChoice() {
     else {alert("Input Error."); return "Error";}
 }
 
-let humanScore = 0;
-let computerScore = 0;
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-function playRound(humanChoice, computerChoice) {
-    console.log(`You chose: ${humanChoice}`);
-    console.log(`Computer chose: ${computerChoice}`);
-    console.log(`Score - Human: ${humanScore}, Computer: ${computerScore}`);
-
-    if(humanChoice === "Error") {console.log("Round skipped due to invalid input."); return "Round skipped due to invalid input.";}
-    if(humanChoice === computerChoice) {return "Draw"}
-    if(humanChoice === "rock"     && computerChoice === "scissors") {humanScore    ++; console.log("Human wins");    return("human wins");   }
-    if(humanChoice === "rock"     && computerChoice === "paper"   ) {computerScore ++; console.log("Computer wins"); return("Computer wins");}
-    if(humanChoice === "paper"    && computerChoice === "scissors") {computerScore ++; console.log("Computer wins"); return("Computer wins");}
-    if(humanChoice === "paper"    && computerChoice === "rock"    ) {humanScore    ++; console.log("Human wins");    return("human wins");   }
-    if(humanChoice === "scissors" && computerChoice === "rock"    ) {computerScore ++; console.log("Computer wins"); return("Computer wins");}
-    if(humanChoice === "scissors" && computerChoice === "paper"   ) {humanScore    ++; console.log("Human wins");    return("human wins");   }
-}
-playRound(humanSelection, computerSelection);
-
 function playGame() {
-    
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        console.log(`You chose: ${humanChoice}`);
+        console.log(`Computer chose: ${computerChoice}`);
+
+        if(humanChoice === "Error") {console.log("Round skipped due to invalid input."); return "Round skipped due to invalid input.";}
+        if(humanChoice === computerChoice) {return "Draw"}
+        if(
+            (humanChoice === "rock"     && computerChoice === "scissors")||
+            (humanChoice === "paper"    && computerChoice === "rock"    )||
+            (humanChoice === "scissors" && computerChoice === "paper"   )
+        ) {humanScore ++; console.log(`Score - Human: ${humanScore}, Computer: ${computerScore}`); return("human wins!"); }
+        else{computerScore ++; console.log(`Score - Human: ${humanScore}, Computer: ${computerScore}`); return "Computer Wins!";}
+    }
+
+    for (let i = 0; i < 5; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        console.log(`You chose: ${humanChoice}.\nComputer chose: ${computerChoice}`)
+        console.log(playRound(humanChoice, computerChoice))
+    }
+
+    console.log(`Final Scores:\nYour score: ${humanScore}\nComputer Score: ${computerScore}`);
+    if (humanScore > computerScore) {
+        console.log("You won the game!");
+    } else if (computerScore > humanScore) {
+        console.log("Computer won the game!");
+    } else {
+        console.log("The game is a tie!");
+    }    
 }
+
+playGame();
 
 //testing:
 console.log("You reached the end!");
